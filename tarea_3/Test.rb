7df568f52792507c56b0a6ee496ea1b1879fb42c
Pedro Antonio Vazquez Rodriguez <pedrovazquezax@users.noreply.github.com>
2017-09-04 08:51:39 -0500

    .
    .

--- tarea_3/Test.rb
+++ tarea_3/Test.rb
@@ -6,12 +6,18 @@ menu=Menu.new
 loop do
   a=menu.DespliegaMenu("\nQue desea hacer\n1.-Complelmento A1\n2.-Complemento A2\n3.-Convertir de binario a decimal\n4.-Salir\nOpcion:" ,4)
   if a == 1
-    
+    print("#{bin.complementA1(gets.chomp).join("")}\n")
   elsif a == 2
 
   elsif a ==3
+    print("#{bin.binToDec(gets.chomp)}\n")
 
   else
     break
   end
 end
+
+
+#print("complelento\n")
+#print("#{complementA1(gets.chomp).join("")}\n")
+print("#{binToDec(gets.chomp)}\n")
